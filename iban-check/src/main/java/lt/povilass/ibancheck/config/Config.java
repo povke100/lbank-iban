72f56c0db7d4f935d4ba9743ae2dc9eb481d183d
Povilas Sinkevičius <sinkevicius.povilas@gmail.com>
2019-12-15 03:09:44 +0200

    Added configuration loading

--- iban-check/src/main/java/lt/povilass/ibancheck/config/Config.java
+++ iban-check/src/main/java/lt/povilass/ibancheck/config/Config.java
@@ -1,5 +1,30 @@
 package lt.povilass.ibancheck.config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Config {
+	
+private static Properties properties = new Properties();
+	
+	static {
+		try {	
+			if(Files.exists(Paths.get("./currencyrates.properties"))) {
+				log.debug("Loading properties from external file");
+				properties.load(new FileInputStream("./currencyrates.properties"));
+			} else {
+				log.debug("Loading properties from classpath config file...");
+				properties.load(Config.class.getClassLoader().getResourceAsStream("currencyrates.properties"));
+			}
+		} catch (IOException e) {
+			log.error("Loading of properties has failed.", e);
+		}
+	}
 
 }
