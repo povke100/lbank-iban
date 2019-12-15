f5b6e606c4aea18451ed8a0cfd0907da8b0fd5fa
Povilas Sinkevičius <sinkevicius.povilas@gmail.com>
2019-12-15 12:47:08 +0200

    Implemented length validation

--- iban-check/src/main/java/lt/povilass/ibancheck/util/ValidationUtil.java
+++ iban-check/src/main/java/lt/povilass/ibancheck/util/ValidationUtil.java
@@ -22,21 +22,23 @@ public class ValidationUtil {
 	/**
 	 * Method checks if given BBAN format is correct for given country
 	 * 
-	 * @param iban
+	 * @param iban 
 	 * @return true if BBAN matched the configured format, returns false if
 	 *         otherwise.
 	 */
-	public static boolean validateBBAN(IBAN iban) throws Exception {
+	public static boolean validateBBAN(String bban, String ccode) throws Exception {
 
-		log.debug("validateBBAN. IBAN:{}", iban);
+		log.debug("validateBBAN. BBAN:{}, Country code: {}", bban, ccode);
 
-		String[] format = Configuration.getBBANFormatByCountry(iban.getCountryCode()).split(",");
+		String[] format = Configuration.getBBANFormatByCountry(ccode).split(",");
 		if (format.length == 0) {
 
-			log.error("validateBBAN. BBAN configuration for country {} was not found!", iban.getCountryCode());
-			throw new Exception("BBAN configuration for country " + iban.getCountryCode() + " was not found!");
+			log.error("validateBBAN. BBAN configuration for country {} was not found!", ccode);
+			throw new Exception("BBAN configuration for country " + ccode + " was not found!");
 		}
 
+		log.debug("validateBBAN. format: {}", Configuration.getBBANFormatByCountry(ccode));
+		
 		int beginIndex = 0;
 
 		// if configuration is found every section of BBAN is checked
@@ -47,27 +49,27 @@ public class ValidationUtil {
 
 			if (!secLenghtS.matches(ConstUtil.RGX_NUMERICAL) || !secFormat.matches(ConstUtil.RGX_BBAN_FORMAT)) {
 
-				log.error("validateBBAN. Invalid BBAN configuration {} for {}", section, iban.getCountryCode());
-				throw new Exception("Invalid BBAN configuration " + section + " for country: " + iban.getCountryCode());
+				log.error("validateBBAN. Invalid BBAN configuration {} for {}", section, ccode);
+				throw new Exception("Invalid BBAN configuration " + section + " for country: " + ccode);
 			}
 
 			int secLenght = Integer.valueOf(secLenghtS);
 
 			switch (secFormat) {
 			case ConstUtil.ALPHABETICAL:
-				if (!iban.getBban().substring(beginIndex, secLenght).matches(ConstUtil.RGX_ALPHABETICAL)) {
+				if (!bban.substring(beginIndex, secLenght).matches(ConstUtil.RGX_ALPHABETICAL)) {
 					return false;
 				}
 				beginIndex += secLenght;
 				break;
 			case ConstUtil.ALPHANUMERICAL:
-				if (!iban.getBban().substring(beginIndex, secLenght).matches(ConstUtil.RGX_ALPHANUMERICAL)) {
+				if (!bban.substring(beginIndex, secLenght).matches(ConstUtil.RGX_ALPHANUMERICAL)) {
 					return false;
 				}
 				beginIndex += secLenght;
 				break;
 			case ConstUtil.NUMERICAL:
-				if (!iban.getBban().substring(beginIndex, secLenght).matches(ConstUtil.RGX_NUMERICAL)) {
+				if (!bban.substring(beginIndex, secLenght).matches(ConstUtil.RGX_NUMERICAL)) {
 					return false;
 				}
 				beginIndex += secLenght;
@@ -77,12 +79,31 @@ public class ValidationUtil {
 
 		return true;
 	}
+	
+	
+	/**
+	 * Checks if given IBAN has correct length
+	 * @param iban IBAN to be checked
+	 * @param ccode Contry code 
+	 * @return true if lenght of iban is equals 
+	 * @throws Exception if lenght for given country code is missing or 
+	 */
+	public static boolean validateLength(String iban, String ccode) throws Exception {
 
-	public static boolean validateLength(String iban) {
-
-		return false;
+		String ibanLenght = Configuration.getIBANLenghtByCountry(ccode);
+		if (ibanLenght.length() == 0 || ibanLenght.matches(ConstUtil.RGX_NUMERICAL)) {
+			log.error("validateLength. Missing or invalid iban lenght parameter for country: {}!", ccode);
+			throw new Exception("Missing or invalid iban lenght parameter for country: " + ccode);
+		}
+		return iban.length() == Integer.valueOf(ibanLenght);
 	}
 
+	/**
+	 * 
+	 * @param ccode
+	 * @return
+	 * @throws Exception
+	 */
 	public static boolean validateCountryCode(String ccode) throws Exception {
 
 		if (Configuration.getCountryCodes().length == 0) {
